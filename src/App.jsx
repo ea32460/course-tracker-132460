5d8986484fcdd9e2de09151a40fdca45155136a0
ea32460 <ea32460@seeu.edu.mk>
2026-04-16 17:08:09 +0200

    Created project

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,121 @@
-import { useState, useCallback } from "react";
-import StudentCard from "./components/StudentCard";
-import RegisterCourse from "./components/RegisterCourse";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
 function App() {
-
-  const [courses, setCourses] = useState([
-    {
-      id: 1,
-      name: "Client Side Programming",
-      credits: 6,
-      grade: 10,
-      attending: true,
-      difficulty: "Moderate"
-    },
-    {
-      id: 2,
-      name: "Operating Systems",
-      credits: 6,
-      grade: 9,
-      attending: true,
-      difficulty: "Hard"
-    },
-    {
-      id: 3,
-      name: "Databases",
-      credits: 6,
-      grade: 7,
-      attending: false,
-      difficulty: "Moderate"
-    },
-    {
-      id: 4,
-      name: "Applied Probability and Statistics",
-      credits: 6,
-      grade: 8,
-      attending: true,
-      difficulty: "Hard"
-    },
-    {
-      id: 5,
-      name: "Written Communication",
-      credits: 3,
-      grade: 10,
-      attending: true,
-      difficulty: "Easy"
-    },
-    {
-      id: 6,
-      name: "English for IT",
-      credits: 3,
-      grade: 9,
-      attending: true,
-      difficulty: "Easy"
-    }
-  ]);
-
-  const addCourse = useCallback((course) => {
-    setCourses(prev => [...prev, course]);
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-      <div className="container">
-
-        <h1 className="title">Semester Course Tracker</h1>
-
-        <div className="student-info">
-          <h2>Emine</h2>
-          <p>ID: 132460</p>
-          <p>These are my enrolled courses for this semester:</p>
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
-
-        <div className="courses">
-          {courses.map(course => (
-              <StudentCard key={course.id} course={course} />
-          ))}
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
         </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
 
-        <RegisterCourse addCourse={addCourse} />
+      <div className="ticks"></div>
 
-      </div>
-  );
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
 }
 
-export default App;
-
+export default App
